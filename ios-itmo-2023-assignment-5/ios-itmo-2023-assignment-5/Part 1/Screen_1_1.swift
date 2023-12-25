8a3c9c991292eeb0ef7aa538122af7c0a51a1296
Stepan Shimigonov <shstepanspb@gmail.com>
2023-12-26 00:25:53 +0300

    hw 5

--- ios-itmo-2023-assignment-5/ios-itmo-2023-assignment-5/Part 1/Screen_1_1.swift
+++ ios-itmo-2023-assignment-5/ios-itmo-2023-assignment-5/Part 1/Screen_1_1.swift
@@ -1,9 +1,56 @@
 import SwiftUI
 
+struct MyButton: View {
+    var action: () -> Void
+    var text: String
+    
+    var body: some View {
+        Button(action: action) {
+            Text(text)
+                .font(.system(size: 17, weight: .medium, design: .default))
+                .padding()
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .background(Color(red: 38/255, green: 136/255, blue: 235/255))
+                .foregroundColor(.white)
+                .cornerRadius(10)
+        }
+    }
+}
+
 struct Screen_1_1: View {
+    @State private var url: String = "vk.com"
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack(alignment: .center) {
+                Text("QR-код")
+                    .font(.system(size: 21, weight: .bold, design: .default))
+                TextField("vk.com", text: $url)
+                    .frame(height: 44)
+                    .padding(.horizontal, 12)
+                    .background(Color(red: 242/255, green: 243/255, blue: 245/255))
+                    .cornerRadius(10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.black.opacity(0.12), lineWidth: 0.5)
+                    )
+                    .padding(.bottom, 44)
+                Image("QR")
+                
+            }
+            .padding(.horizontal, 20)
+            .frame(minHeight: 0, maxHeight: .infinity)
+            
+            HStack {
+                MyButton(action: {}, text: "PNG")
+                MyButton(action: {}, text: "SVG")
+            }
+            .frame(minHeight: 0, maxHeight: 68)
+            .padding(.horizontal, 12)
+            .padding(.vertical, 12)
+            
+        }
+            
     }
 
 }
--- ios-itmo-2023-assignment-5/ios-itmo-2023-assignment-5/Part 2/Screen_2_1.swift
+++ ios-itmo-2023-assignment-5/ios-itmo-2023-assignment-5/Part 2/Screen_2_1.swift
@@ -1,11 +1,103 @@
 import SwiftUI
 
 struct Screen_2_1: View {
-
+    let images = ["wall1", "wall2", "wall3"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Image("back")
+                Spacer()
+                Image("cart")
+                Image("more")
+                    .padding(.trailing, 4)
+            }
+            
+            TabView {
+                ForEach(images, id: \.self) { img in
+                    Image(img)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                }
+            }
+            .tabViewStyle(PageTabViewStyle())
+            .onAppear() {
+                setUpAppearence()
+            }
+            .frame(maxHeight: 375)
+        
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Стена в ВК")
+                        .font(.system(size: 20, weight: .regular, design: .default))
+                    HStack {
+                        Text("99999999999 ₽")
+                            .font(.system(size: 20, weight: .medium, design: .default))
+                        Text("0 ₽")
+                            .font(.system(size: 16, weight: .regular, design: .default))
+                            .foregroundColor(Color(red: 129/255, green: 140/255, blue: 153/255))
+                            .strikethrough()
+                            .padding(.top, 4)
+                    }
+                }
+                .padding(.leading, 12)
+                .padding(.vertical, 20)
+                Spacer()
+                Image("bookmark")
+            }
+            Divider()
+                .padding(.horizontal, 10)
+                .foregroundColor(Color(red: 129/255, green: 140/255, blue: 153/255))
+            VStack(alignment: .leading) {
+                Text("Доставка")
+                    .font(.system(size: 16, weight: .regular, design: .default))
+                Text("Никогда не доставим, Санкт-Петербург — сюда точно нет.")
+                    .font(.system(size: 13, weight: .regular, design: .default))
+                    .foregroundColor(Color(red: 129/255, green: 140/255, blue: 153/255))
+                Text("Бесплатная доставка была в 2006.")
+                    .font(.system(size: 13, weight: .regular, design: .default))
+                    .foregroundColor(Color(red: 129/255, green: 140/255, blue: 153/255))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .padding(.leading, 12)
+            .padding(.top, 5)
+            .background()
+            
+            Spacer()
+            
+            VStack {
+                Button(action: {
+                }) {
+                    Text("Добавить в корзину")
+                        .font(.system(size: 17, weight: .medium, design: .default))
+                        .padding()
+                        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                        .background(Color(red: 38/255, green: 136/255, blue: 235/255))
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+                
+                Button(action: {
+                }) {
+                    Text("Написать")
+                        .font(.system(size: 17, weight: .medium, design: .default))
+                        .padding()
+                        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                        .background(Color(red: 242/255, green: 243/255, blue: 245/255))
+                        .foregroundColor(Color(red: 38/255, green: 136/255, blue: 235/255))
+                        .cornerRadius(10)
+                }.padding(.top, 4)
+            }
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+            .padding(.horizontal, 20)
+        }
     }
+}
 
+private func setUpAppearence() {
+    UIPageControl.appearance().currentPageIndicatorTintColor = UIColor(Color.gray)
+    UIPageControl.appearance().pageIndicatorTintColor = UIColor.gray.withAlphaComponent(0.2)
 }
 
 #Preview {
